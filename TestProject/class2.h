83de631718c75334a1c338fbf6c0a2df9b5442fc
Janne Colliander <janne.colliander@gmail.com>
2017-02-08 18:19:47 +0200

    Forgot class2 header

---
+++ TestProject/class2.h
@@ -0,0 +1,12 @@
+#include <string>
+
+using namespace std;
+
+class class2
+{
+        public:
+        int value;
+        int method1(int value);
+	string method2(string name);
+	string method3(string name);
+};
