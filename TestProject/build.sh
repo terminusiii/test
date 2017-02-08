dd97942fc87dbc6e231363304a4d7cad2a545cd4
Janne Colliander <janne.colliander@gmail.com>
2017-02-08 17:38:50 +0200

    Forgot to add these to last commit. New build script for project.

---
+++ TestProject/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "**********************"
+echo "Builds app and app.exe"
+echo "**********************"
+make -f Makefile clean
+make -f Makefile
+make -f MakefileWindows clean
+make -f MakefileWindows
+echo "Run with ./app on Linux or"
+echo "app.exe on Windows"
+
---
+++ TestProject/class2.cpp
@@ -0,0 +1,16 @@
+#include "class2.h"
+#include <string>
+
+int class2::method1(int value) { return value + 1; }
+string class2::method2(string name)
+{
+	return name + " " + string("sukunimi");
+}
+
+string class2::method3(string name)
+{
+	return name + " - " + string("Colliander");
+}
+
+
+
