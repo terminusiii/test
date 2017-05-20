84e368be17cb3da87f76e8cd76f527771269e14d
Janne Colliander <janne.colliander@gmail.com>
2017-05-20 22:20:05 +0300

    Added JavaTestproject

---
+++ TestProject/JavaTestproject/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld
+{
+	public static void main(String[] args)  {
+		// Prints Hellow world
+		System.out.println("Hello, World");
+		System.out.println("Hello, World again");
+	}
+}
