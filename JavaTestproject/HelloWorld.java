24fc401a80a7e5f288e41642efa2e50750335b8c
Janne Colliander <janne.colliander@gmail.com>
2017-05-22 09:49:41 +0300

    Moved JavaTestProject

---
+++ JavaTestproject/HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld
+{
+
+	public static void main(String[] args)  {
+		// Prints Hellow world
+		System.out.println("Hello, World");
+		System.out.println("Hello, World again");
+		
+		Test();
+	}
+
+	public static void Test()	{
+		System.out.println("Tadaa! At Test() - function");
+	}
+}
